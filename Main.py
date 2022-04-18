d472543f2bf96bc3a96f64db91fcc0d68916bf80
doryosi <dorsinai1004@gmail.com>
2022-04-18 09:11:30 +0000

    remove var from get_device_details function

--- Main.py
+++ Main.py
@@ -7,8 +7,7 @@ from netmiko.ssh_exception import AuthenticationException
 
 # read the file "device_file" and create a dictionary
 def get_device_details():
-    file_path = "/var/lib/jenkins/workspace/Git_Job/Configuration_BackUp/devices_details"
-    with open(file_path) as f:
+    with open("devices_details") as f:
         content = csv.reader(f)
         dict_from_csv = {rows[0]: rows[1:6] for rows in content}
     return dict_from_csv
